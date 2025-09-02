1e2020b771528d0920a008e1f5f626e8458663e6
hemo hemooo <eaboargob@gmail.com>
2025-09-02 20:28:43 +0000

    Service worker (background banner)
    You already have one; make sure it u

--- public/firebase-messaging-sw.js
+++ public/firebase-messaging-sw.js
@@ -1,8 +1,7 @@
-/* global self, clients */
+/* global self, importScripts, firebase */
 importScripts("https://www.gstatic.com/firebasejs/10.12.4/firebase-app-compat.js");
 importScripts("https://www.gstatic.com/firebasejs/10.12.4/firebase-messaging-compat.js");
 
-// Paste your web config here:
 firebase.initializeApp({
   "projectId": "routemaster-admin-k1thy",
   "appId": "1:1071157867562:web:e6a8686b2849d515157f53",
@@ -13,11 +12,11 @@ firebase.initializeApp({
 });
 
 const messaging = firebase.messaging();
+
 messaging.onBackgroundMessage(({ notification, data }) => {
-  self.registration.showNotification(notification?.title || "RouteMaster", {
-    body: notification?.body,
-    data,
-    icon: "/icon-192.png",
-    badge: "/badge.png",
+  const title = (notification && notification.title) || "RouteMaster";
+  const body  = (notification && notification.body) || "";
+  self.registration.showNotification(title, {
+    body, data, icon: "/icon-192.png", badge: "/badge.png"
   });
 });
